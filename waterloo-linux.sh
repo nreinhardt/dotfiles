799f9bd085c18d1c6c4443e966f59eb243080e13
Noah Reinhardt <ndreinha@uwaterloo.ca>
2017-01-09 20:29:09 -0500

    don't need pycharm

--- waterloo-linux.sh
+++ waterloo-linux.sh
@@ -3,5 +3,4 @@
 ###############################################################################
 
 alias ls="~/software/ls-colors/bin/ls"
-alias pycharm="/u6/ndreinha/software/pycharm-2016.3.2/bin/pycharm.sh &"
 alias mips="/u4/cs251/Simulator/mips"
