db42e4b1f6133fcc80fe59bf10dd9c8dfd6b60cb
Noah Reinhardt <ndreinha@uwaterloo.ca>
2017-01-09 20:28:30 -0500

    AUTO: push all files

--- waterloo-linux.sh
+++ waterloo-linux.sh
@@ -2,4 +2,6 @@
 # LS-COLORS
 ###############################################################################
 
-alias ls="~/utilities/ls-colors/bin/ls"
+alias ls="~/software/ls-colors/bin/ls"
+alias pycharm="/u6/ndreinha/software/pycharm-2016.3.2/bin/pycharm.sh &"
+alias mips="/u4/cs251/Simulator/mips"
