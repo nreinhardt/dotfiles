e9b6a52bf961fa4d02d13d1fb0bf57be58172614
Noah Reinhardt <ndrein@gmail.com>
2017-01-10 20:33:06 -0500

    Don't use manual FASD in general (requires installation)

--- general.sh
+++ general.sh
@@ -30,18 +30,6 @@ export EDITOR=vim
 
 
 ###############################################################################
-# FASD
-###############################################################################
-
-# Initialize [fasd](https://github.com/clvv/fasd/wiki)
-eval "$(fasd --init auto)"
-# Use 'v' to open any file
-alias v="f -e vim"
-# Autocomplete 'v'
-_fasd_bash_hook_cmd_complete v
-
-
-###############################################################################
 # LESS
 ###############################################################################
 
@@ -59,7 +47,5 @@ alias push='git add -A && git commit -m "AUTO: push all files" && git push'
 # ALIAS
 ###############################################################################
 
-# List most recent last
-alias ll="ls -l --human-readable -g --no-group -t --reverse"
 alias grep="egrep"
 alias rsync="rsync --progress --human-readable --recursive"
